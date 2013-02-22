5e854e87353d0ec8226b9479a95ae26c442a127f
Nils Ole Timm <funkinperson@googlemail.com>
2013-02-22 17:22:13 +0100

    More comfort changes: -Update Content script now only updates changed files

--- src/UpdateContent.bat
+++ src/UpdateContent.bat
@@ -1,2 +1,2 @@
-xcopy /I /E /Y "Platformer\Platformer\bin\x86\Debug\Content" "MapEditor\bin\Debug\Content"
-xcopy /I /E /Y "Platformer\Platformer\bin\x86\Debug\Content" "IGORR Server\bin\Debug\Content"
+xcopy /I /E /Y /D "Platformer\Platformer\bin\x86\Debug\Content" "MapEditor\bin\Debug\Content"
+xcopy /I /E /Y /D "Platformer\Platformer\bin\x86\Debug\Content" "IGORR Server\bin\Debug\Content"
